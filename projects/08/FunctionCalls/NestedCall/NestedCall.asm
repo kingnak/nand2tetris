323fa1734b596d35585ae66e5ad080b38b2da4cd
unknown <philip.masser@PC-172_W10.asim-at.local>
2017-05-12 12:28:49 +0200

    new artefact with new translator

--- projects/08/FunctionCalls/NestedCall/NestedCall.asm
+++ projects/08/FunctionCalls/NestedCall/NestedCall.asm
@@ -1,163 +1,120 @@
-
-// function Sys.init 0
+@Sys.halt
+D=A
+@256
+M=D
+D=A
+A=A+1
+M=D+1
+@ARG
+M=D
+@261
+D=A
+@SP
+M=D
+@LCL
+M=D
+@THIS
+M=0
+@THAT
+M=0
+@Sys.init
+0;JMP
 (Sys.init)
-
-// push constant 4000
 @4000
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop pointer 0
 @SP
 AM=M-1
 D=M
 @3
 M=D
-
-// push constant 5000
 @5000
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop pointer 1
 @SP
 AM=M-1
 D=M
 @4
 M=D
-
-// call Sys.main 0
-@_return_Sys.0
+@Sys.main_return_0
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-@LCL
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@ARG
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@THIS
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@THAT
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@SP
-D=M
-@5
-D=D-A
-@ARG
-M=D
-@SP
-D=M
-@LCL
-M=D
 @Sys.main
+D=A
+@R13
+M=D
+@0
+D=A+1
+@_call
 0;JMP
-(_return_Sys.0)
-
-// pop temp 1
+(Sys.main_return_0)
 @SP
 AM=M-1
 D=M
 @6
 M=D
-
-// label LOOP
-(LOOP)
-
-// goto LOOP
-@LOOP
+(Sys.init$LOOP)
+@Sys.init$LOOP
 0;JMP
-
-// function Sys.main 5
 (Sys.main)
+@Sys.main_init_0
+D=A
+@R13
+M=D
 @5
 D=A
-(_func_init_Sys.0)
-@SP
-M=M+1
-A=M-1
-M=0
-@_func_init_Sys.0
-D=D-1;JGT
-
-// push constant 4001
+@_init_lcl
+0;JMP
+(Sys.main_init_0)
 @4001
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop pointer 0
 @SP
 AM=M-1
 D=M
 @3
 M=D
-
-// push constant 5001
 @5001
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop pointer 1
 @SP
 AM=M-1
 D=M
 @4
 M=D
-
-// push constant 200
 @200
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop local 1
 @SP
 AM=M-1
 D=M
 @LCL
 A=M+1
 M=D
-
-// push constant 40
 @40
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop local 2
 @LCL
 D=M
 @2
@@ -170,16 +127,12 @@ D=M
 @R13
 A=M
 M=D
-
-// push constant 6
 @6
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop local 3
 @LCL
 D=M
 @3
@@ -192,68 +145,32 @@ D=M
 @R13
 A=M
 M=D
-
-// push constant 123
 @123
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// call Sys.add12 1
-@_return_Sys.1
+@Sys.add12_return_0
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-@LCL
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@ARG
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@THIS
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@THAT
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-@SP
-D=M
-@6
-D=D-A
-@ARG
-M=D
-@SP
-D=M
-@LCL
-M=D
 @Sys.add12
+D=A
+@R13
+M=D
+@1
+D=A+1
+@_call
 0;JMP
-(_return_Sys.1)
-
-// pop temp 0
+(Sys.add12_return_0)
 @SP
 AM=M-1
 D=M
 @5
 M=D
-
-// push local 0
 @LCL
 A=M
 D=M
@@ -261,8 +178,6 @@ D=M
 M=M+1
 A=M-1
 M=D
-
-// push local 1
 @LCL
 A=M+1
 D=M
@@ -270,8 +185,6 @@ D=M
 M=M+1
 A=M-1
 M=D
-
-// push local 2
 @LCL
 D=M
 @2
@@ -281,8 +194,6 @@ D=M
 M=M+1
 A=M-1
 M=D
-
-// push local 3
 @LCL
 D=M
 @3
@@ -292,8 +203,6 @@ D=M
 M=M+1
 A=M-1
 M=D
-
-// push local 4
 @LCL
 D=M
 @4
@@ -303,8 +212,6 @@ D=M
 M=M+1
 A=M-1
 M=D
-
-// add
 @SP
 AM=M-1
 D=M
@@ -314,8 +221,6 @@ D=D+A
 @SP
 A=M-1
 M=D
-
-// add
 @SP
 AM=M-1
 D=M
@@ -325,8 +230,6 @@ D=D+A
 @SP
 A=M-1
 M=D
-
-// add
 @SP
 AM=M-1
 D=M
@@ -336,8 +239,6 @@ D=D+A
 @SP
 A=M-1
 M=D
-
-// add
 @SP
 AM=M-1
 D=M
@@ -347,85 +248,31 @@ D=D+A
 @SP
 A=M-1
 M=D
-
-// return
-@LCL
-D=M
-@R13
-M=D
-@5
-A=D-A
-D=M
-@R14
-M=D
-@SP
-A=M-1
-D=M
-@ARG
-A=M
-M=D
-@ARG
-D=M+1
-@SP
-M=D
-@R13
-AM=M-1
-D=M
-@THAT
-M=D
-@R13
-AM=M-1
-D=M
-@THIS
-M=D
-@R13
-AM=M-1
-D=M
-@ARG
-M=D
-@R13
-AM=M-1
-D=M
-@LCL
-M=D
-@R14
-A=M
+@_return
 0;JMP
-
-// function Sys.add12 0
 (Sys.add12)
-
-// push constant 4002
 @4002
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop pointer 0
 @SP
 AM=M-1
 D=M
 @3
 M=D
-
-// push constant 5002
 @5002
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// pop pointer 1
 @SP
 AM=M-1
 D=M
 @4
 M=D
-
-// push argument 0
 @ARG
 A=M
 D=M
@@ -433,16 +280,12 @@ D=M
 M=M+1
 A=M-1
 M=D
-
-// push constant 12
 @12
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-
-// add
 @SP
 AM=M-1
 D=M
@@ -452,8 +295,52 @@ D=D+A
 @SP
 A=M-1
 M=D
-
-// return
+@_return
+0;JMP
+(Sys.halt)
+@Sys.halt
+0;JMP
+(_call)
+@SP
+D=-D
+D=D+M
+@R14
+M=D
+@LCL
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+@ARG
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+@THIS
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+@THAT
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+@R14
+D=M
+@ARG
+M=D
+@SP
+D=M
+@LCL
+M=D
+@R13
+A=M;JMP
+(_return)
 @LCL
 D=M
 @R13
@@ -494,5 +381,13 @@ D=M
 @LCL
 M=D
 @R14
-A=M
-0;JMP
+A=M;JMP
+(_init_lcl)
+@SP
+M=M+1
+A=M-1
+M=0
+@_init_lcl
+D=D-1;JGT
+@R13
+A=M;JMP
